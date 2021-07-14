7b7d14fd3e37ee186a94e66a9e54f31832302ff6
Erik Bjäreholt <erik@bjareho.lt>
2021-07-14 15:39:23 +0200

    fix: fixed config fallback for both quantifiedme and qslang
    fix: fixed config fallback for both quantifiedme and qslang

--- src/quantifiedme/config.py
+++ src/quantifiedme/config.py
@@ -8,10 +8,18 @@ import appdirs
 logger = logging.getLogger(__name__)
 
 
+srcdir = Path(__file__).resolve().parent.parent
+rootdir = srcdir.parent
+
+
 def load_config() -> MutableMapping[str, Any]:
     filepath = Path(appdirs.user_config_dir("quantifiedme")) / "config.toml"
     if not filepath.exists():
         logger.warning("No config found, falling back to example config")
-        filepath = Path(appdirs.user_config_dir("quantifiedme")) / "config.toml.example"
+        filepath = Path(rootdir) / "config.toml.example"
     with open(filepath) as f:
         return toml.load(f)
+
+
+if __name__ == "__main__":
+    load_config()

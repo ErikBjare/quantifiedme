358a8808988d9fef04262a8071be36aed7d3de6a
Erik Bjäreholt <erik@bjareho.lt>
2021-07-14 15:25:23 +0200

    fix: fallback to example config if real config missing

--- src/quantifiedme/config.py
+++ src/quantifiedme/config.py
@@ -1,10 +1,17 @@
 from typing import MutableMapping, Any
 from pathlib import Path
+import logging
+
 import toml
 import appdirs
+
+logger = logging.getLogger(__name__)
 
 
 def load_config() -> MutableMapping[str, Any]:
     filepath = Path(appdirs.user_config_dir("quantifiedme")) / "config.toml"
+    if not filepath.exists():
+        logger.warning("No config found, falling back to example config")
+        filepath = Path(appdirs.user_config_dir("quantifiedme")) / "config.toml.example"
     with open(filepath) as f:
         return toml.load(f)

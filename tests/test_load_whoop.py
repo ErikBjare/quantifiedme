75de0190070397caff1fa28647db29c333063450
Erik Bjäreholt <erik@bjareho.lt>
2023-11-21 12:12:23 +0100

    test: skip whoop tests if no config

--- tests/test_load_whoop.py
+++ tests/test_load_whoop.py
@@ -1,11 +1,17 @@
+import pytest
+from quantifiedme.config import load_config
 from quantifiedme.load.whoop import load_heartrate_df, load_sleep_df
 
+has_whoop_config = load_config().get("data", {}).get("whoop", False)
 
+
+@pytest.mark.skipif(has_whoop_config, reason="no whoop config")
 def test_load_whoop_heartrate():
     df = load_heartrate_df()
     print(df.head())
 
 
+@pytest.mark.skipif(has_whoop_config, reason="no whoop config")
 def test_load_whoop_sleep():
     df = load_sleep_df()
     print(df.head())
